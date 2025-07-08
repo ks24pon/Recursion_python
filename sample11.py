88f4bb58ee99609f3f9c9cac61716ff29eba4a91
ks24pon <pom123re0624@gmail.com>
2025-07-08 14:22:23 +0900

    https://recursionist.io/dashboard/problems/384

---
+++ sample11.py
@@ -0,0 +1,17 @@
+myPower = 300
+mySpeed = 200
+myTechnique = 50
+myDefense = 500
+
+# ここから書きましょう
+myPower += 10
+mySpeed -= 5
+myTechnique *= 7
+myDefense //= 2
+
+
+print("myPower : " + str(myPower))
+print("mySpeed : " + str(mySpeed))
+print("myTechnique : " + str(myTechnique))
+print("myDefense : " + str(myDefense))
+# Python では str(int) で int 型を文字列へ変換します
